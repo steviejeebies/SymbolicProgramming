248e90b54b74b7d6a9082ff9ee417cc23f318d33
Stephen Rowe <rowes@tcd.ie>
2020-10-31 04:55:18 +0000

    legal done

--- Untitled-1.pl
+++ Untitled-1.pl
@@ -10,3 +10,5 @@ incr(f1(null), f0(f1(null))).
 incr(f0(X), f1(X)).
 incr(f1(X), f0(Y)) :- incr(X, Y), X \= null. % this is not the arithmetic predicate =/=
 
+legal(f0(null)).
+legal(X):- legal(Y), incr(Y, X).
