9c398352906075ad108cc0b42b71cec8d0312160
Stephen Rowe <rowes@tcd.ie>
2020-10-31 20:59:13 +0000

    added comments

--- Untitled-1.pl
+++ Untitled-1.pl
@@ -5,26 +5,38 @@ pterm(f1(X)) :- pterm(X).
 reverse(f0(P1), f1(P1)).
 reverse(f1(P1), f0(P1)).
 
+% Question 1
+
 incr(null, f1(null)).
 incr(f1(null), f0(f1(null))).
 incr(f0(X), f1(X)).
 incr(f1(X), f0(Y)) :- incr(X, Y), X \= null. % this is not the arithmetic predicate =/=
+
+% Question 2
 
 legal(f0(null)).
 legal(X):- legal(Y), incr(Y, X).
 
 incrR(X, Y):- legal(X), incr(X, Y).
 
+% Question 3
+
 add(null, X, X).
 add(f0(null), X, X).
 add(X, Y, Z) :- incr(A, X), add(A, Y, B), incr(B, Z).
+
+% Question 4
 
 mult(null, _, f0(null)). % for X * 0 = 0 (if 0 is defined as null)
 mult(f0(null), _, f0(null)). % (if 0 is defined as f0(null))
 mult(f1(null), X, X).
 mult(X, Y, Z) :- incr(A, X), mult(A, Y, B), add(Y, B, Z). 
 
+% Question 5
+
 revers(X,Y) :- rev(X, Y, null).
 rev(f0(X), Y, Z) :- rev(X, Y, f0(Z)).
 rev(f1(X), Y, Z) :- rev(X, Y, f1(Z)).
 rev(null, Z, Z).
+
+% Question 6
