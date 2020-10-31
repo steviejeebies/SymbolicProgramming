4ada46ef746571b3c3c04a704e4914c63c6aa8d3
Stephen Rowe <rowes@tcd.ie>
2020-10-31 05:53:19 +0000

    mult done

--- Untitled-1.pl
+++ Untitled-1.pl
@@ -18,3 +18,8 @@ incrR(X, Y):- legal(X), incr(X, Y).
 add(null, X, X).
 add(f0(null), X, X).
 add(X, Y, Z) :- incr(A, X), add(A, Y, B), incr(B, Z).
+
+mult(null, _, f0(null)). % for X * 0 = 0 (if 0 is defined as null)
+mult(f0(null), _, f0(null)). % (if 0 is defined as f0(null))
+mult(f1(null), X, X).
+mult(X, Y, Z) :- incr(A, X), mult(A, Y, B), add(Y, B, Z). 
